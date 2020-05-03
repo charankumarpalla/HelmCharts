6de6a57a02f4f709c9e59c5bfad2a078517c5126
CharanKumar <charankumar.palla@gmail.com>
2020-05-03 10:35:12 +0530

    update for IKS

---
+++ practice/MoFoSo/charan/ibm-mobilefoundation/inventory/ibmcloudEnablement/files/install/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2019. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
+
+echo "uninstall.sh"
